1a480ca9a662efa894d47d1664b1cbbba60ea339
hxld <hxld88888@163.com>
2022-08-15 11:05:09 +0800

    Initial commit

---
+++ order-service-consumer/src/main/java/com/atguigu/gmall/service/OrderService.java
@@ -0,0 +1,12 @@
+package com.atguigu.gmall.service;
+
+/**
+ * @author hxld
+ * @create 2022-08-15 10:48
+ */
+public interface OrderService {
+    /**
+     * 初始化订单
+     */
+    public  void initOrder(String userId);
+}
---
+++ order-service-consumer/src/main/java/com/atguigu/gmall/service/impl/OrderServiceImpl.java
@@ -0,0 +1,14 @@
+package com.atguigu.gmall.service.impl;
+
+import com.atguigu.gmall.service.OrderService;
+
+/**
+ * @author hxld
+ * @create 2022-08-15 10:50
+ */
+public class OrderServiceImpl implements OrderService {
+    @Override
+    public void initOrder(String userId) {
+        //1.查询用户的收货地址
+    }
+}
